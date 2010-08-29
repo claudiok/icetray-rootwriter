4437aafbaf85a228eac06a60006a77ec3f0a930d
Fabian Kislat <fabian.kislat@desy.de>
2010-08-29 20:18:11 +0000

    Add a really simple example script, as suggested by Tilo
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/rootwriter/trunk@66499 16731396-06f5-0310-8873-f7f720988828

---
+++ resources/examples/book_data.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+
+import sys
+
+from I3Tray import *
+
+from icecube.tableio import I3TableWriter
+from icecube.rootwriter import I3ROOTTableService
+from icecube import dataclasses, linefit
+
+load("libdataio")
+
+outfile = sys.argv[1]
+infiles = sys.argv[2:]
+
+tray = I3Tray()
+
+# Read .i3 file
+tray.AddModule("I3Reader", "reader",
+               FileNameList = infiles
+               )
+
+# Open output file
+table_service = I3ROOTTableService(outfile)
+
+# Book data
+tray.AddModule(I3TableWriter, "writer",
+               TableService = table_service,
+               Keys = [ "LineFit", "LineFitParams" ]
+               )
+
+tray.AddModule("TrashCan", "trash")
+
+tray.Execute()
+tray.Finish()
+
