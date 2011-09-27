50046e5c1d5930670f421f140df7dbf2be43eb38
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-09-27 11:40:47 +0000

    Fancify root and hdfwriter segments' icetray-inspect output.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/rootwriter/trunk@80107 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -3,19 +3,15 @@ from icecube import icetray, tableio
 
 load_pybindings(__name__, __path__)
 
-@icetray.traysegment
+@icetray.traysegment(parent=tableio.I3TableWriter, removeopts=('TableService',))
 def I3ROOTWriter(tray, name, Output=None, **kwargs):
 	"""Tabulate data to a ROOT file.
 
-	Arguments:
-		Output: Path to output file
-		Keys: Key names of frame objects to tabulate
-		Streams: Subevent streams to include
-		(See I3TableWriter documentation for others)
+	:param Output: Path to output file
 	"""
 	tabler = I3ROOTTableService(Output)
 	tray.AddModule(tableio.I3TableWriter, name, TableService=tabler,
 	    **kwargs)
 
 # clean the local dictionary
-del icetray,tableio
+del icetray
