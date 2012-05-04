522eb11be6fde942ebccd4afb782b396ac326abf
Alex Olivas <olivas@icecube.umd.edu>
2012-05-04 17:19:19 +0000

    reverting back to loading projects manually.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/rootwriter/trunk@87951 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -11,6 +11,7 @@
  * @author Fabian Kislat <fabian.kislat@desy.de> Last changed by: $LastChangedBy$
  */
 
+#include <icetray/load_project.h>
 
 namespace bp = boost::python;
 
@@ -23,6 +24,8 @@ BOOST_PP_SEQ_FOR_EACH(I3_REGISTRATION_FN_DECL, ~, REGISTER_THESE_THINGS)
 
 I3_PYTHON_MODULE(rootwriter)
 {
+  load_project("rootwriter", false); 
+
   bp::import("icecube.tableio");
 
   BOOST_PP_SEQ_FOR_EACH(I3_REGISTER, ~, REGISTER_THESE_THINGS);
